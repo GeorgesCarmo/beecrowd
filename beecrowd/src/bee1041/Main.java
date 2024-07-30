1898fdc879b989ad6f280fe8a266629cdd6c35b6
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-07-30 16:49:57 -0300

    Update Main.java
    update code

--- beecrowd/src/bee1041/Main.java
+++ beecrowd/src/bee1041/Main.java
@@ -1,9 +1,27 @@
 package bee1041;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double x = scan.nextDouble();
+		double y = scan.nextDouble();
+		
+		if(x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		}else if(x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		}else if(x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		}else if(x > 0.0 && y < 0.0) {
+			System.out.println("Q4");
+		}else if(x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		}
+		
+		scan.close();
 
 	}
 
